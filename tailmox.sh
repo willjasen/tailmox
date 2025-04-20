acbbb58f80f44b1bbe2c58cee916c609defa3242
willjasen <brandon@willjasen.com>
2025-04-20 06:05:05 -0400

    try smaller interval

--- tailmox.sh
+++ tailmox.sh
@@ -204,7 +204,7 @@ function report_peer_latency() {
     # Calculate average latency for each peer
     echo "$peers" | jq -r '.[]' | while read -r peer_ip; do
         echo -e "${BLUE}Calculating average latency for $peer_ip...${RESET}"
-        avg_latency=$(ping -c 10 "$peer_ip" | awk -F'/' 'END {print $5}')
+        avg_latency=$(ping -i 0.1 -c 10 "$peer_ip" | awk -F'/' 'END {print $5}')
         if [ -n "$avg_latency" ]; then
             echo -e "${GREEN}Average latency to $peer_ip: ${avg_latency} ms${RESET}"
         else
