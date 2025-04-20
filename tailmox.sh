bccbf146d2f7f5d07bf0663b690f6e2c7c6f730a
willjasen <brandon@willjasen.com>
2025-04-20 16:26:47 -0400

    checkout a tag

--- tailmox.sh
+++ tailmox.sh
@@ -106,8 +106,9 @@ function start_tailscale() {
 
 # Run Tailscale certificate services
 function run_tailscale_cert_services() {
-    git clone --branch v1.0.0 https://github.com/willjasen/tailscale-cert-services /opt/tailscale-cert-services;
+    git clone https://github.com/willjasen/tailscale-cert-services /opt/tailscale-cert-services;
     cd /opt/tailscale-cert-services;
+    git checkout tags/v1.0.0
     ./proxmox-cert.sh;
     cd /opt/tailmox;
 }
