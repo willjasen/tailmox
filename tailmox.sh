6a7c73026cdb2623466c90566aa6ed6829c31d83
willjasen <brandon@willjasen.com>
2025-10-14 22:40:57 -0400

    use bc to calculate ping timespan

--- tailmox.sh
+++ tailmox.sh
@@ -267,7 +267,7 @@ function ensure_ping_reachability() {
         local peer_hostname=$(tailscale status --json | jq -r ".Peer[] | select(.TailscaleIPs[0] == \"$peer_ip\") | .HostName")
         local ping_interval=0.25
         local ping_count=25
-        local ping_span=$(($ping_interval * $ping_count))
+        local ping_span=$(echo "$ping_interval * $ping_count" | bc)
 
         log_echo "${BLUE}Pinging $peer_hostname ($peer_ip) ($ping_count pings over $ping_span seconds)...${RESET}"
         if ! ping -c $ping_count -i $ping_interval -W 1 "$peer_ip" | grep -q "0 received"; then
