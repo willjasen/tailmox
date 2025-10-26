3fa63d0f09eb1fbf09328bd69778088c8cda957e
willjasen <brandon@willjasen.com>
2025-10-26 18:14:56 -0400

    remove double quotes for tar path

--- test-env/download-template.sh
+++ test-env/download-template.sh
@@ -119,7 +119,7 @@ decompress_xz() {
   echo "Decompressing $src -> $dst"
   if [[ "$src" == *.tar.xz ]]; then
     if command -v tar >/dev/null 2>&1; then
-      tar -xJf "$src"
+      tar -xJf $src
       return $?
     else
       echo "tar/xz is not available to decompress $src" >&2
